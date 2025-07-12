4037c5c57e2b94f6531d8bacaab08a1e15590da8
wpzhicay <wpzhicay@sudamericano.edu.ec>
2025-07-11 21:22:45 -0500

    Primer commit desde frontend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private registerUrl = 'http://localhost:3000/users/register';
-  private loginUrl = 'http://localhost:3000/users/login';
+  private registerUrl = `${environment.apiUrl}/users/register`;
+  private loginUrl = `${environment.apiUrl}/users/login`;
 
   constructor(private http: HttpClient) {}
 
   register(data: { name: string; email: string; password: string }): Observable<any> {
     return this.http.post(this.registerUrl, data);
   }
-  
 
   login(data: { email: string; password: string }): Observable<any> {
     return this.http.post(this.loginUrl, data);
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000' // o el puerto que uses localmente
+};
