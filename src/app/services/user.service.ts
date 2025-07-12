81416c86f240524890f8292ed7e872caf55e3bfb
wpzhicay <wpzhicay@sudamericano.edu.ec>
2025-07-11 23:34:05 -0500

    fix: usar environment.apiUrl en AuthService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface RegisterData {
   name: string;
@@ -12,7 +13,7 @@ export interface RegisterData {
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/users/register';
+  private apiUrl = `${environment.apiUrl}/users/register`;
 
   constructor(private http: HttpClient) {}
 
